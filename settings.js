a44ebef87f18802691b966e7aa849d1193b40f2b
david-create-beep <willzee2500@gmail.com>
2025-03-08 20:44:01 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,7 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "shadow garden by david
-  ",
+  botName: "shadow garden by david",
   botOwner: 'David', // Your name
   ownerNumber: '9135302884', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
