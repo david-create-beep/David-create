f17b6c0edc3f8bcb75bdef5bd2dd701d3c9fe0eb
david-create-beep <willzee2500@gmail.com>
2025-03-08 19:22:38 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,10 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "shadow garden by david
+  ",
+  botOwner: 'David', // Your name
+  ownerNumber: '9135302884', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
